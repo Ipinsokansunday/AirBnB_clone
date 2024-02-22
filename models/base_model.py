7cde7080366ab83fc82c5bd82c97ed9206f1fad0
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-02-22 09:00:40 -0800

    0x00. AirBnB clone - The console

--- models/base_model.py
+++ models/base_model.py
@@ -8,16 +8,16 @@ from datetime import datetime
 class BaseModel:
     """Represents the BaseModel of the HBnB project."""
     def __init__(self, *args, **kwargs):
-    """Initialize a new BaseModel instance.
+        """Initialize a new BaseModel instance.
 
-    Args:
-        *args (any): Unused.
-        **kwargs (dict): Key/value pairs of attributes.
-    """
-    timestamp_format = "%Y-%m-%dT%H:%M:%S.%f"
-    self.id = str(uuid4())
-    self.created_at = datetime.today()
-    self.updated_at = datetime.today()
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        timestamp_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
     if kwargs:
         for k, v in kwargs.items():
             if k in ("created_at", "updated_at"):
